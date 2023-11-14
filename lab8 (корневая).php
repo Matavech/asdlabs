cad9cfc1c42af8b95602f84bfb4910d8a98f7717
VolkovMatvey <matavech@icloud.com>
2023-11-14 17:48:07 +0200

    labs

--- lab8 (корневая).php
+++ lab8 (корневая).php
@@ -1,30 +1,26 @@
 <?php
-function rootSort($arr) {
-	$maxValue = max($arr);
-	$minValue = min($arr);
-	$range = $maxValue - $minValue + 1;
 
-	// Создаем массив для подсчета вхождений каждого элемента
-	$count = array_fill(0, $range, 0);
+function radixSort(array &$arr) {
+	$max = max($arr);
+	$numDigits = strlen((string)$max);
 
-	// Подсчитываем вхождения элементов
-	foreach ($arr as $element) {
-		$count[$element - $minValue]++;
-	}
+	for ($i = 0; $i < $numDigits; $i++) {
+		$buckets = array_fill(0, 10, []);
 
-	// Строим отсортированный массив на основе подсчетов
-	$sortedArray = [];
-	for ($i = 0; $i < $range; $i++) {
-		while ($count[$i] > 0) {
-			$sortedArray[] = $i + $minValue;
-			$count[$i]--;
+		foreach ($arr as $value) {
+			$digit = ($value / pow(10, $i)) % 10;
+			$buckets[$digit][] = $value;
+		}
+
+		$arr = [];
+		foreach ($buckets as $bucket) {
+			$arr = array_merge($arr, $bucket);
 		}
 	}
-
-	return $sortedArray;
 }
 
+// Пример использования
+$array = [170, 45, 75, 90, 802, 24, 2, 66];
+radixSort($array);
 
-$arr = [4, 2, 2, 8, 3, 3, 1];
-$sortedArray = rootSort($arr);
-echo "Отсортированный массив: " . implode(", ", $sortedArray);
+echo "Отсортированный массив: " . implode(", ", $array);
--- lab9 (пирамидальная).php
+++ lab9 (пирамидальная).php
@@ -1,55 +1,51 @@
 <?php
 
-function buildHeap($arr, $n, $root) {
-	$largest = $root;
-	$leftChild = 2 * $root + 1;
-	$rightChild = 2 * $root + 2;
-
-	// Находим наибольший элемент среди корня и его детей
-	if ($leftChild < $n && $arr[$leftChild] > $arr[$largest]) {
-		$largest = $leftChild;
-	}
-
-	if ($rightChild < $n && $arr[$rightChild] > $arr[$largest]) {
-		$largest = $rightChild;
-	}
-
-	if ($largest != $root) {
-		// Если наибольший элемент не корень, меняем их местами
-		$temp = $arr[$root];
-		$arr[$root] = $arr[$largest];
-		$arr[$largest] = $temp;
-
-		// Рекурсивно строим стопку для дочернего поддерева
-		$arr = buildHeap($arr, $n, $largest);
-	}
-
-	return $arr;
-}
-
-function heapSort($arr) {
+function heapSort(&$arr)
+{
 	$n = count($arr);
 
-	// Строим начальное дерево (перегруппируем массив)
-	for ($i = $n / 2 - 1; $i >= 0; $i--) {
-		$arr = buildHeap($arr, $n, $i);
+	// Строим max-кучу
+	for ($i = ($n / 2) - 1; $i >= 0; $i--) {
+		heapify($arr, $n, $i);
 	}
 
-	// Извлекаем элементы из стопки по одному и помещаем их в конец массива
-	for ($i = $n - 1; $i >= 0; $i--) {
-		// Меняем местами корень стопки (наибольший элемент) с последним элементом в массиве
+	// Постепенно извлекаем элементы из кучи
+	for ($i = $n - 1; $i > 0; $i--) {
+		// Меняем корень кучи с последним элементом массива
 		$temp = $arr[0];
 		$arr[0] = $arr[$i];
 		$arr[$i] = $temp;
 
-		// Рекурсивно строим стопку для уменьшенной части массива
-		$arr = buildHeap($arr, $i, 0);
+		heapify($arr, $i, 0);
 	}
-
-	return $arr;
 }
 
-$array = [12, 11, 13, 5, 6];
-$sortedArray = heapSort($array);
+function heapify(&$arr, $n, $i)
+{
+	$largest = $i;
+	$left = 2 * $i + 1;
+	$right = 2 * $i + 2;
 
-echo "Отсортированный массив: " . implode(", ", $sortedArray);
+	if ($left < $n && $arr[$left] > $arr[$largest]) {
+		$largest = $left;
+	}
+
+	if ($right < $n && $arr[$right] > $arr[$largest]) {
+		$largest = $right;
+	}
+
+	if ($largest !== $i) {
+		$temp = $arr[$i];
+		$arr[$i] = $arr[$largest];
+		$arr[$largest] = $temp;
+
+		// Рекурсивно применяем heapify к измененной подкуче
+		heapify($arr, $n, $largest);
+	}
+}
+
+// Пример использования
+$arr = [12, 11, 13, 5, 6, 7];
+heapSort($arr);
+
+echo "Отсортированный массив: " . implode(", ", $arr);
